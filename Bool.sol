d10904545b37568782e672a45f3dba8f4cab59aa
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-07-28 13:26:46 +0300

    bool def

---
+++ Bool.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT  
+pragma solidity ^0.8.30;
+import "./node_modules/hardhat/console.sol"; // to can write console.log()
+
+contract MyContract{
+    //default value = false 
+    bool MyBool = true ;
+    //1 byte
+}
