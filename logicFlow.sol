6f68bd5ee205b022b99ab09791084f0617589703
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-01 20:49:45 +0300

    getGradeFunction

--- logicFlow.sol
+++ logicFlow.sol
@@ -7,4 +7,19 @@ contract if_statement{
             return"positive";
         }
          return "nigative";
-    }}
+    }
+    function getGrade(uint256 grade) public pure returns (string memory){
+        if (grade>90){
+            return "A";
+        }  
+        else if (grade>80){
+            return "B";
+        }  
+        else if (grade>70){
+            return "C";
+        }  
+        else if (grade>60){
+            return "D";
+        }
+        return "Fail" ; 
+        }}
