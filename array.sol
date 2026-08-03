4ac2cb247bc59fb6551d2e1e065194b524d064b9
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-03 22:22:31 +0300

    add array_def contract with fixed size and dynamic array operations

---
+++ array.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+import "hardhat/console.sol";
+
+// Array defenation 
+contract array_def{
+    uint256[4] public nums=[1,1,3,8]; // fixed size array
+    uint256[] arr = [9,3,4,5];
+    function getIndex(uint256 _index) public view returns(uint256){
+        return arr[_index];
+    }
+    function addElement(uint256 _num) public {
+        arr.push(_num);
+    }
+    function getLength() public view returns(uint256){
+        return arr.length;
+    }
+    function delete_Last_Element()public {
+        arr.pop();
+    }
+}
