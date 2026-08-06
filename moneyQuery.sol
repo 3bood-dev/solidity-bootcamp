7f4b663c18fc6d50541117b5df4b614b2ec1893e
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-06 20:57:02 +0300

    add send_func contract to facilitate fund transfers using transfer, send, and call methods

--- moneyQuery.sol
+++ moneyQuery.sol
@@ -19,3 +19,26 @@ contract receive_func {
         // let you receive money on contract without spending any money on functions 
     } //let the property __> calldata : empty 
 }
+//___________________________________________________
+//how can i send money from contract to user 
+// using ( transfer , send , call   )
+contract send_func{
+    address owner;
+    constructor(){
+        owner = msg.sender;
+    }
+    function fund() public payable {
+
+    }
+    function using_transfer() public {
+        payable(owner).transfer(0.5 ether);
+    }
+    function using_send()public{
+       bool success = payable(owner).send(0.5 ether);
+       require(success , "transfer failed");
+    }
+    function using_call() public {
+        (bool success,) = payable(owner).call{value : 0.5 ether}("");
+        require (success, "transfer failed!");
+    }
+}
