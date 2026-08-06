c7c1e164583541ba51a96b4c262b9195654bb876
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-06 20:47:08 +0300

    add Event_def2 contract with ticket purchase logic and event emission for ticket sales

--- events.sol
+++ events.sol
@@ -13,3 +13,21 @@ contract Event_def{
     emit ticketSold(msg.sender, PRICE);
   } 
 }
+
+contract Event_def2{
+    uint256 public PRICE = 100;
+    //MAX indexed is 3 in the event 
+    // 
+  event ticketSold(address indexed,uint256 indexed); 
+  // when we use indexed something called topics[] in the logs have the values of event 
+  // like this :
+  // topics[0] = hash event
+  // topics[1] = first item
+  // topics[2] = sec item 
+  function buyTicket()public {
+    //logic
+    //
+    //
+    emit ticketSold(msg.sender, PRICE);
+  } 
+}
