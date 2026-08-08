489b8a2392cabfdd9e076622f23e6b69314aa140
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-08 20:41:17 +0300

    add IAnimal interface and Dog contract with setSound implementation

---
+++ Interface.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+//we use interface over abstract contract --> when we put all functions without implemntations 
+
+// and we use abstract contract when we have atleast a one function have no implemntations
+
+// How we type the syntax ??
+// use "interface" and make the next word have 'I' in the fisrt of it --> (convintion) like IAnimal 
+// and use external instead of (public virtual) but we use "override" normaly in the childs 
+
+interface IAnimal{
+    //have no state variable 
+    //have no constructor 
+    //can inherite from anthor interface 
+    //can use (user defined types) like (enum,structs)
+    function setSound() external pure returns(string memory);
+}
+
+contract Dog is IAnimal{
+    function setSound() public pure returns(string memory){
+        return "Woof";
+    }
+}
